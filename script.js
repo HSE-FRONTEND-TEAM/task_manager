e75b4af31a906ca6395fc857de71daa7846e3b27
yosheeeee <evantipin@edu.hse.ru>
2023-10-25 01:14:19 +0500

    add popup menu Co-authored-by: TkMaxim9 <TkMaxim9@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,30 @@
+let html = document.querySelector('html')
+let body = document.querySelector("body")
+let theme_btn = document.querySelector('.theme')
+html.style.setProperty('--current_theme', "dark")
+theme_btn.addEventListener('click', () => {
+    if (html.style.getPropertyValue('--current_theme') == "dark") {
+        html.style.setProperty('--bg_color', "var(--bg_color_light)")
+        html.style.setProperty('--theme_button', "var(--theme_button_light)")
+        html.style.setProperty('--bg_task', 'var(--bg_task_light)')
+        html.style.setProperty('--current_theme', "light")
+    } else {
+        html.style.setProperty('--current_theme', "dark")
+        html.style.setProperty('--bg_color', "var(--bg_color_dark)")
+        html.style.setProperty('--theme_button', "var(--theme_button_dark)")
+        html.style.setProperty('--bg_task', 'var(--bg_task_dark)')
+    }
+})
+
+let create_task_form = document.querySelector('.new_task_form')
+let create_task_layout = document.querySelector('.new_task_layout')
+create_task_form.addEventListener('submit', (event) => {
+    create_task_layout.style.display = "none"
+    let newElem = document.createElement('h1')
+    newElem.style.color = "white"
+    let taskDescription = create_task_form.querySelector('input')
+    let radionButton = create_task_form.querySelector('input[name="category"]:checked')
+    newElem.innerHTML = `${taskDescription.value}, type= ${radionButton.value}`
+    body.append(newElem)
+    event.preventDefault()
+})
